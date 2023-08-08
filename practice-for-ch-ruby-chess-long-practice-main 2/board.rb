e3166528e25deaeff82106343c37866d8a81103f
Brittiny Filbert <britfilbert@icloud.com>
2023-08-08 14:28:33 -0700

    Add phase one

--- practice-for-ch-ruby-chess-long-practice-main 2/board.rb
+++ practice-for-ch-ruby-chess-long-practice-main 2/board.rb
@@ -2,28 +2,44 @@ require_relative "piece.rb"
 
 class Board
     attr_accessor :rows
-    def initialize()
-        @board = Array.new(8) {Array.new(8, Piece.new)}
-        @board.set
+    def initialize(
+    )
+        @board = Array.new(8) {Array.new(8, Piece.new("P"))}
+        # self.set
     end
 
     def [](pos) # [2,2]
-        row, column = pos
+        row, col = pos
         @board[row][col]
     end
 
     def []=(pos, val)
-        row, column = pos
-        @board[row][column] = val
+        row, col = pos
+        @board[row][col] = val
     end
 
     def move_piece(start_pos, end_pos)
-        @board[end_pos] = @board[start_pos]
-        @board[start_pos] = nil
+        self[end_pos]= self[start_pos]
+        self[start_pos]= nil
     end
 
-    private
+    # private
     def set
-        
+
+        row = 2
+        while row < 6
+            col = 0
+            while col < 8
+                @board[row][col]= nil
+                col += 1
+            end
+            row += 1
+        end
+    end
+
+    def print
+        @board.each do |row|
+            
+        end
     end
 end
--- practice-for-ch-ruby-chess-long-practice-main 2/piece.rb
+++ practice-for-ch-ruby-chess-long-practice-main 2/piece.rb
@@ -1,3 +1,8 @@
 class Piece
-    
+
+    # attr_reader :name
+
+    def initialize(name)
+        @name = name
+    end
 end
